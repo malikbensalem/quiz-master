9e2843ecae9f1fbc21dda14078a77c652b41ce8e
malik <malikbensalem@outlook.com>
2022-08-31 11:15:38 +0100

    added getQuestionnaireCategories

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -17,17 +17,34 @@ function timedRedirect(link,time=2500){
 }
 function emptyTableMsg(ele,msg){
 	$(ele).html('<tr><td class="text-center" colspan="100%">'+msg+'</td></tr>')
-
+}
+function getQuestionnaireCategories(){
+	$.ajax({
+        method: 'GET',
+        url: 'xhr.php',
+        data: {
+            action:'get_questionnaire_categories',
+        },
+        dataType: 'json',
+        success: function(data) {
+        	if (data.result){
+        		data.cats.forEach(function(c){
+            		$('#category-filter').append($('<option>', {
+					    value: c.id,
+					    text: c.desc,
+					}));
+        		})
+        	}
+    	}
+    })
 }
 $('.disabled,.active').click(function(e){
 	e.preventDefault();
 	e.stopPropagation();
 	return
 })
-
-
 $('.table-responsive').on('show.bs.select', function () { 
 	$('.table-responsive').css( "overflow", "inherit" );
 	$('.bootstrap-table').css( "overflow", "inherit" ); 
 	$('.fixed-table-body').css( "overflow", "inherit" );     
-}); 
+});

80e87c8dd60ccee828f26d78eeca835b99c41fa3
malik <malikbensalem@outlook.com>
2022-08-31 08:53:14 +0100

    created user sessions on register and login

--- auth/verify.php
+++ auth/verify.php
@@ -5,11 +5,10 @@ if (isPost()){
 	if ($_POST['action']=='login'){
 		$em=$_POST['em'];
 		$pw=hashPW($em,$_POST['pw']);
-		$noUser=mysqli_query($mysqli,"SELECT u_id,u_us_id,u_ut_id FROM users WHERE u_email='$em' AND u_password='$pw'");
-		if (mysqli_num_rows($noUser)==0){
+		$sts=setUserSession($em,$pw)
+		if ($sts==false){
 			die(json_encode(['result'=>false,'alert'=>'<div class="alert alert-danger">Wrong email or password.</div>']));
 		}
-		list($_SESSION['user_id'],$sts,$_SESSION['user_level'])=mysqli_fetch_row($noUser);
 		if ($sts!=1){
 			die(json_encode(['result'=>false,'alert'=>'<div class="alert alert-danger">This user has been removed.</div>']));
 		}
@@ -23,7 +22,17 @@ if (isPost()){
 		$ln=ucfirst($_POST['ln']);
 
 		mysqli_query($mysqli,"INSERT INTO users (u_email,u_password,u_first_name,u_last_name) VALUES('$em','$pw','$fn','$ln')") or die(json_encode(['result'=>false,'alert'=>'<div class="alert alert-danger"> This email is already registered with us.</div>']));
+		setUserSession($em,$pw);
 
 		die(json_encode(['result'=>true,'alert'=>'<div class="alert alert-success">Successfully registered.</div>']));
 	}
 }
+function setUserSession($em,$pw){
+	$noUser=mysqli_query($mysqli,"SELECT u_id,u_us_id,u_ut_id FROM users WHERE u_email='$em' AND u_password='$pw'");
+	if (mysqli_num_rows($noUser)==0){
+		return fasle; 
+	}
+	list($_SESSION['user_id'],$sts,$_SESSION['user_level'])=mysqli_fetch_row($noUser);
+	return $sts;
+}
+amended

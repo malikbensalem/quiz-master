f9c8c3573db3f72a9308ed7823044d8d1115a9f2
malik <malikbensalem@outlook.com>
2022-09-01 10:30:15 +0100

    added hasAccess function

--- questionnaire/index.php
+++ questionnaire/index.php
@@ -24,18 +24,20 @@ if (!loggedin()){
 						<a class="btn btn-lg btn-block btn-outline-info active">Action</a>
 						<button class="btn btn-lg btn-block btn-outline-dark" id="create-q">Create questionnaire</button>
 					</div>
+					<?
 
+					?>
+					<?if(hasAccess('2')){?>
 					<div class="btn-group-vertical btn-block" id="statuses">
 						<a class="btn btn-lg btn-block btn-outline-info active">Status</a>
 						<button class="btn btn-lg btn-block btn-outline-dark" data-btn-status="1">Published</button>
-						
-						<?if($_SESSION['user_level']==1){?><button class="btn btn-lg btn-block btn-outline-dark" data-btn-status="complete">Complete</button><?}?>
-						<?if($_SESSION['user_level']>1){?><button class="btn btn-lg btn-block btn-outline-dark"data-btn-status="2">Drafts</button><?}?>
+						<button class="btn btn-lg btn-block btn-outline-dark"data-btn-status="2">Drafts</button>
 					</div>
+					<?}?>
 					<div class="btn-group-vertical btn-block">
 						<a class="btn btn-lg btn-block btn-outline-info active">Filters</a>
 						<select class="form-control form-select-lg selectpicker " title="Assign filter" id="assigned-filter" data-style="btn-outline-dark">
-							<option value="1" selected>Assigned <?if($_SESSION['user_level']>2){?>/ owned<?}?></option>
+							<option value="1" selected>Assigned <?if(hasAccess('2')){?>/ owned<?}?></option>
 							<option value="2">Not assigned</option>
 						</select>
 						<?if($_SESSION['user_level']==1){?>
@@ -52,7 +54,7 @@ if (!loggedin()){
 				<div class="col-md-8">
 					<div class="table-responsive">
 						<table class="table table-striped " id="tbl-questionnaire">
-							<thead><th>Title</th><th>Subject</th><th>Pass</th><?if($_SESSION['user_level']==1){?><th>Passed</th><?}?><th>Assigned by</th><?if($_SESSION['user_level']>1){?><th>Owned by</th><?}?><th>Actions</th></thead>
+							<thead><th>Title</th><th>Subject</th><?if($_SESSION['user_level']==1){?><th>Passed</th><?}?><th>Assigned by</th><th>Deadline</th><?if(hasAccess('2')){?><th>Owned by</th><?}?><th>Actions</th></thead>
 							<tbody></tbody>
 						</table>
 					</div>
@@ -72,6 +74,7 @@ if (!loggedin()){
 			getQuestionnaires(1)
 			getQuestionnaireCategories()
 			function getQuestionnaires(sts,cat=[],assign=1,complete=1){
+
 				loader(true)
 				$.ajax({
 	                method: 'GET',
@@ -89,14 +92,13 @@ if (!loggedin()){
 	                	if (data.result){
 	                		
 	                		data.quest.forEach(function(q){
-								$('#tbl-questionnaire tbody').append('<tr data-id='+q.id+'><td data-title>'+q.title+'</td><td data-cat="'+q.cid+'">'+q.cat+'</td><td>'+q.pass+'</td><?if ($_SESSION['user_level']==1){?><td>'+(q.passed==1?'<i class="fa-solid fa-check"></i>':'<i class="fa-solid fa-x"></i>')+'<?}?></td><td>'+q.assigned+'</td><?if($_SESSION['user_level']>1){?><td>'+q.owned+'</td><?}?><td><div class="btn-group"><?if ($_SESSION['user_level']>1){?><button class="btn btn-light" data-edit title="Edit"><i class="fa-regular fa-pen-to-square"></i></button><button class="btn btn-dark" data-users title="Assignees"><i class="fa-solid fa-users"></i></button><?}?> <a href="show_questions?id='+q.id+'" class="btn btn-primary" title="Start"><i class="fa-solid fa-play"></i></a></div></td></tr>')
+								$('#tbl-questionnaire tbody').append('<tr data-id='+q.id+'><td data-title>'+q.title+'</td><td data-cat="'+q.cid+'">'+q.cat+'</td><?if ($_SESSION['user_level']==1){?><td>'+(q.passed==1?'<i class="fa-solid fa-check"></i>':'<i class="fa-solid fa-x"></i>')+'<?}?></td><td>'+q.assigned+'</td><td>'+q.deadline+'</td><?if(hasAccess('2')){?><td>'+q.owned+'</td><?}?><td><div class="btn-group"><?if (hasAccess('2')){?><button class="btn btn-light" data-edit title="Edit"><i class="fa-regular fa-pen-to-square"></i></button><button class="btn btn-dark" data-users title="Assignees"><i class="fa-solid fa-users"></i></button><?}?> <a href="show_questions?id='+q.id+'" class="btn btn-primary" title="Start"><i class="fa-solid fa-play"></i></a></div></td></tr>')
 	                		})
 	                	}
 	                	else{
 	                		emptyTableMsg('#tbl-questionnaire tbody','There are no questionnaires')
 	                	}
-					loader(false)
-
+						loader(false)
 	                }
 				})
 			}
@@ -109,9 +111,9 @@ if (!loggedin()){
 				getQuestionnaires($('#statuses button[data-btn-status].active').data('btn-status'),$('#category-filter').val(),$('#assigned-filter').val(),$('#complete-filter').val())
 			})
 			$('#statuses button[data-btn-status]').click(function(){
-				getQuestionnaires($('#statuses button[data-btn-status].active').data('btn-status'),$('#category-filter').val(),$('#assigned-filter').val(),$('#complete-filter').val())
+				getQuestionnaires($(this).data('btn-status'),$('#category-filter').val(),$('#assigned-filter').val(),$('#complete-filter').val())
 			})
-			<?if ($_SESSION['user_level']>1){?>
+			<?if(hasAccess('2')){?>
 			$('#tbl-questionnaire tbody').on('click','button[data-users]',function(){
 				$('#user-assign-modal').data('qid',$(this).parents('tr').data('id'))
 				$('#user-assign-modal').modal('show')
